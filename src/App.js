5e48bd26f17d29a3f5513acf47be6bf6f7baee64
prince owire <princeowire131@gmail.com>
2023-11-23 20:50:12 +0100

    completed

---
+++ src/App.js
@@ -0,0 +1,177 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App"> 
+      <nav>
+      <img src=".\img\eGENCY.svg" alt="" />
+           <div className='nav-details'>
+        <a href="./">About</a>
+        <a href="./">Product</a>
+        <a href="./">Contact</a>
+        <a href="./">Help</a>
+      </div>
+  
+    <div>
+      <button className='sign-in'>sign in</button>
+      <button className='sign-up'>sign up</button>
+    </div>
+    </nav>
+  
+  <section className='sec-1'>
+    <div className='img-1'><img src=".\img\Saly-3.png" alt="sally-3" /></div>
+    <div className='sec-1-div'></div>
+  <div className='sec-1-div'>
+    <h1> We scratch, build and play together </h1>
+    <p>Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum. Est audiam animal molestiae te. Ex duo eripuit mentitum.</p>
+    <button className='Contact-Us'> Contact Us </button>
+  </div>
+  </section>
+
+  <section className="sec-2"> 
+  <img className='doodle-3' src=".\img\doodle-3 1.svg" alt="" />
+  <div className='div-grp1'>
+    <p className='bobo'>Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum.Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum.</p>
+    <p>Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum.Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum.</p>
+    <p>Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum.Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum.</p>
+  </div>
+  <div><img src=".\img\man.png" alt="bright" /></div>
+  </section>
+
+  <section className="sec-3">
+    <h3>Simplify your online shipping for free this month!</h3>
+    <img  className='sally-5'  src=".\img\Saly-5.png" alt="bike man" />
+    <h3>Grow your business with better Shipping organizations</h3>
+  </section>
+
+  <section className='sec-4'>
+    <div className='sec-4sub-1'>
+      <div><p>Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum.</p></div>
+      <div><p>Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum.</p></div>
+    </div>
+
+    <div className='sec-4sub-2'>
+      <div><p>Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum.</p></div>
+      <div><p>Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum.</p></div>
+      <div><p>Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum.</p></div>
+      <div><p>Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum.</p></div>
+    </div>
+  </section>
+
+  <section className="sec-5">
+    <div> <img src=".\img\Saly-22.png" alt=" of a girl" /> </div>
+    <div>
+      <p>We scratch, build and play together</p>
+      <h3>We scratch, build and play together</h3>
+      <p className='sec-5-small-p'>Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum. Est audiam animal molestiae te. Ex duo eripuit mentitum.</p>
+    </div>
+  </section>
+
+  <section className="sec-6">
+    <img className='sally-6' src=".\img\Saly-6.png" alt="sally-6" />
+    <div>
+      <h3>Grow your business with better <br /> Shipping organizations</h3>
+      <div className='sec-6-btn-div'>
+        <input type="text" placeholder='write your maill here' />
+        <button>Subscribe</button>
+      </div>
+    </div>
+  </section>
+
+  <section className='sec-7'>
+    <div className='sub-sec-7-1'>
+      <h3>The best design for your <br />studio website</h3>
+      <a href='./'>Experienced team </a>
+    </div>
+
+    <div className='sub-sec-7-2'>
+      <div>
+        <h3>Audit</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing Ac aliquam ac volutpat, viverra magna risus aliquam massa. Ac aliquam ac volutpat, viverra magna risus. </p>
+        <button className='btn-yellow'>Experienced team</button>
+      </div>
+      <div>
+        <h3>Team Work</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing Ac aliquam ac volutpat, viverra magna risus aliquam massa. Ac aliquam ac volutpat, viverra magna risus. </p>
+        <button className='btn-green'>Experienced team</button>
+      </div>
+      <div>
+        <h3>Create</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing Ac aliquam ac volutpat, viverra magna risus aliquam massa. Ac aliquam ac volutpat, viverra magna risus. </p>
+        <button className='btn-yellow'>Experienced team</button>
+      </div>
+    </div>
+  </section>
+
+  <footer>
+    <img className='doodle-7-1' src=".\img\doodle-7 1.svg" alt="doodle" />
+    <div className='ft-part-1'>
+
+    <div>
+      <img src=".\img\eGENCY.svg" alt="egency" />
+      <p>
+      Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum. Est audiam animal molestiae te. Ex duo eripuit mentitum. Et has minim elitr intellegat. Mea aeterno eleifend antiopam ad, nam no suscipit quaerendum. At nam minimum ponderum. Est audiam animal molestiae te. Ex duo eripuit mentitum.
+      </p>
+    </div>
+
+    <div>
+      <h5>pages</h5>
+      <div>
+        <p>Temlate</p>
+        <p>Price</p>
+        <p>Help</p>
+        <p>Contact</p>
+      </div>
+    </div>
+
+    <div>
+      <h5>Demos</h5>
+      <div>
+        <p>Dark Theme</p>
+        <p>Light Theme</p>
+        <p>Clasic Theme</p>
+      </div>
+    </div>
+
+    <div>
+      <h5>Resources</h5>
+      <div>
+        <p>Documentation</p>
+      </div>
+    </div>
+
+    </div>
+
+    <div className='ft-part-2'>
+        <div className='abi'>
+          <div>
+            <b>New York</b>
+            <b>Dubai</b>
+            <b>Dhaka</b>
+            <b>Mumbai</b>
+            <b>Sydney</b>
+          </div>
+        </div>
+          <img src=".\img\doodle-6 1.svg" alt="doodle-6 1" className='doodle-6' />
+          <img src=".\img\doodle-5 1.svg" alt="doodle-5 1" className='doodle-5' />
+        <div className='ft-logos'>
+          <img src=".\img\visa.jpg" alt="visa" />
+          <img src=".\img\Group 673.svg" alt="Group" />
+          <img src=".\img\paypal.svg" alt="paypal" />
+          <img src=".\img\mastercard.svg" alt="mastercard" />
+          <img src=".\img\strip.svg" alt="stripe" />
+          <img src=".\img\btc.svg" alt="btc" />
+          <img src=".\img\bancontact.svg" alt="bancontact" />
+        </div>
+      </div>
+
+      <div className='tandc'>
+        <p>© 2019-2020 All Rights Reserved.</p>
+        <p>Terms of Service</p>
+      </div>
+  </footer>
+ </div>
+  )
+}
+export default App;
